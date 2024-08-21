0711bc580ae4a8a91346b951e8b562865172ca2d
minaemad13 <menaemadorai@gmail.com>
2024-08-21 19:48:11 +0300

    edit

--- mechatronics_landingpage/wsgi.py
+++ mechatronics_landingpage/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mechatronics_landingpage.settings')
 
 application = get_wsgi_application()
+
+app=application
